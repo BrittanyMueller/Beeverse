4604f489aa0f7f3be2776ba33c3577b2f5ff7ecf
BrittanyMueller <brittany_mueller@sympatico.ca>
2023-03-15 17:12:02 -0400

    Experimenting with sky domes.

--- Assets/Scripts/Controller/Honeycomb.cs
+++ Assets/Scripts/Controller/Honeycomb.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Honeycomb : MonoBehaviour {
+
+  // honeycomb type num todo
+  
   // Start is called before the first frame update
   void Start() {}
 
